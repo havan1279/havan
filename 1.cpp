e408dcf6074687eab3b245982466895d9075af25
havan1279 <80148417+havan1279@users.noreply.github.com>
2021-03-06 15:20:53 +0700

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a[150][150];
+    int n;
+    cin >> n;
+    for(int i = 0;i<n;i++){
+        for(int j = 0; j < n; j++)
+		cin >> a[i][j];
+    }
+    for(int i = 0;i<n;i++){
+        if 
+    }
+}
---
+++ 2.cpp
@@ -0,0 +1,30 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int i = 2, j, so, kt;
+    cin >> so;
+    while (i <= so) {
+        kt = 1;
+        if (i != 0 && i != 1) {
+            j = 2;
+            while(j <= i/2) {
+                if(i%j == 0) {
+                    kt = 0;
+                    break;
+                }
+                j++;
+            }
+        } else {
+           kt = 0;
+        }
+ 
+        if(kt == 1) {
+            cout << i << " ";
+        }
+         
+       i++;
+    }
+    return 0;
+}
